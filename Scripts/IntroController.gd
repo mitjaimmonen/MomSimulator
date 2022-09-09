dbbdad06c9d3b61090be42c5037264d7da8a0f2a
Mitja <mitja.immonen@protonmail.com>
2022-09-10 01:05:47 +0300

    intro anim

--- Scripts/IntroController.gd
+++ Scripts/IntroController.gd
@@ -1,24 +1,22 @@
 extends Node2D
 
-var timer : float = 0
+onready var animation_player = get_node("AnimationPlayer") 
 
 func _ready():
 	set_process(false)
 	visible = false
 	GameManager.connect("solution_state_changed", self, "_on_solution_state_changed")
 
-func _process(delta):
-	timer += delta
-	if timer > 2:
-		GameManager._set_solution_state(GameManager.SolutionState.GAME)
 
 func _on_solution_state_changed():
 	if GameManager._get_solution_state() == GameManager.SolutionState.INTRO:
 		print("Playing Intro")
 		visible = true
-		timer = 0
-		set_process(true)
+		animation_player.play("intro_anim")
 	else:
 		visible = false
-		set_process(false)
 		
+
+
+func _on_animation_finished(anim_name):
+	GameManager._set_solution_state(GameManager.SolutionState.GAME)
